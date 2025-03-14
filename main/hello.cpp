970f46f3c0674fffca529a068f7b95d4379776d4
nh2seven <nikhil.h@outlook.com>
2025-03-14 12:37:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
+  cout << "I have successfully built and run this as PES1UG22AM105.\n";
   return 0;
 }
