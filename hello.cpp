bf26325e6a008d26c439e0b526347927bef6a316
nh2seven <nikhil.h@outlook.com>
2025-03-14 12:56:31 +0530

    Add hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,9 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    cout << "Hello, Jenkins\n";
+  cout << "I have successfully built a Jenkins pipeline.\n";
+  return 0;
+}
